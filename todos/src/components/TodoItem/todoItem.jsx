27805cf39317608596a159ca8457fb8319d906e4
Hop Nguyen <hopnguyen.dev@gmail.com>
2022-07-27 21:43:33 +0700

    Change status for task item

--- todos/src/components/TodoItem/todoItem.jsx
+++ todos/src/components/TodoItem/todoItem.jsx
@@ -3,39 +3,37 @@ import { useState } from 'react';
 import './styles.css';
 
 const ALL_STATUS = [
-  { name: 'new', order: 0 },
-  { name: 'doing', order: 1 },
-  { name: 'done', order: 2 },
-  { name: 'renew', order: 3 },
+  { status: 'new', order: 0 },
+  { status: 'doing', order: 1 },
+  { status: 'done', order: 2 },
+  { status: 'renew', order: 3 },
 ]
 
 function TodoItem({item, idx}) {
-  const {title, creator, description, status: defaultStatus} = item;
-  const [status, setStatus] = useState(defaultStatus);
+  const [status, setStatus] = useState(item.status);
 
-  const changeStatus = (currentStatus) => {
-    if (currentStatus === 'renew') {
-      return setStatus('new')
-    }
-
-    const foundObj = ALL_STATUS.find((item) => item.name === currentStatus.toLowerCase());
-    const order = foundObj.order;
-    const nextStatus = ALL_STATUS.find((item) => item.order === order + 1).name;
-
-    setStatus(nextStatus);
+  const changeStatus = (e) => {
+     const currentOrder = parseInt(e.target.value);
+     const currentStatus = ALL_STATUS.find(item => item.order === currentOrder).status;
+     setStatus(currentStatus);
   };
 
   return (
     <li className='card' key={idx}>
-      <p className="containerItem__title">Title: {title}</p>
-      <p className="containerItem__creator">Creator: {creator}</p>
-      <p className={`containerItem__status containerItem__status--${status.toLowerCase()}`}> Status: {status}</p>
+      <p className="containerItem__title">Title: {item.title}</p>
+      <p className="containerItem__creator">Creator: {item.creator}</p>
+      <p className={`containerItem__status containerItem__status--${item.status.toLowerCase()}`}> Status: {status}</p>
       <hr className="containerItem__lineBreak" />
       <div className="containerItem__description">
         <p>Description:</p>
-        <p>{description}</p>
+        <p>{item.description}</p>
       </div>
-      <button type='button' onClick={() => changeStatus(status)}>Change</button>
+
+      <select name="" onChange={changeStatus}>
+        {ALL_STATUS.map((item, idx) => {
+          return <option key={idx} value={item.order}>{item.status}</option>
+        })}
+      </select>
     </li>
   );
 }
